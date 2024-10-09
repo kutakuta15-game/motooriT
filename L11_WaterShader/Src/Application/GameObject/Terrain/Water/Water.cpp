9646148ca309b149925e4f48e405bd88d056e978
kutakuta15 <kd1342624@st.kobedenshi.ac.jp>
2024-10-09 11:22:08 +0900

    1009

---
+++ L11_WaterShader/Src/Application/GameObject/Terrain/Water/Water.cpp
@@ -0,0 +1,25 @@
+﻿#include "Water.h"
+
+void Water::Init()
+{
+	if (!m_spModel)
+	{
+		m_spModel = std::make_shared<KdModelWork>();
+		m_spModel->SetModelData("Asset/LessonData/Stage/Water/Water.gltf");
+
+		m_pCollider = std::make_unique<KdCollider>();
+		m_pCollider->RegisterCollisionShape("Water", m_spModel, KdCollider::TypeGround);
+	}
+}
+
+void Water::Update()
+{
+}
+
+void Water::DrawLit()
+{
+	if (!m_spModel) return;
+
+	KdShaderManager::Instance().m_StandardShader.DrawModel(*m_spModel->GetData());
+}
+
---
+++ L11_WaterShader/Src/Application/GameObject/Terrain/Water/Water.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+class Water : public KdGameObject
+{
+public:
+	Water() {}
+	virtual ~Water()		override {}
+
+	void Init()				override;
+	void Update()			override;
+	void DrawLit()			override;
+
+private:
+	std::shared_ptr<KdModelWork> m_spModel = nullptr;
+
+};
